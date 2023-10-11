899d872a8ded53677c2a46146e359e48a79b9a54
Manish <tamangmanish3456@gmail.com>
2023-10-11 12:54:37 +0545

    Build the new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from 'react'
+import {TextField, TextArea, Button} from '@radix-ui/themes';
+export default function NewIssuePage() {
+  return (
+    <div className='max-w-xl space-y-3'>
+      <TextField.Root>
+        <TextField.Input placeholder='Title'  />
+      </TextField.Root>
+        <TextArea placeholder="Description" />
+        <Button>Submit</Button>
+    </div>
+  )
+}
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import {Flex, Text, Button} from '@radix-ui/themes';
+import {Button} from '@radix-ui/themes';
+import Link from 'next/link';
 
 export default function IssuePage() {
   return (
-    <Flex direction={'row'} gap={'2'}>
-      <Text>Hello from Issue :{")"}</Text>
-      <Button>Let's go</Button>
-    </Flex>
+  <div>
+   <Button>
+    <Link href={'/issues/new'}>
+       Add Issue
+    </Link>
+   </Button>
+  </div>
   )
 }
